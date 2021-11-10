6d78d562a019675007ed11e394deb9f1577e93a1
Paweł Czerniak <kontakt@crein.pl>
2021-11-10 21:55:13 +0100

    fetchData ends

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,12 +1,24 @@
 export const fetchData = (url, options = {}) => {
     const {
-        startCallback = () => console.log()
+        startCallback = () => console.log(`${url} Start fetching`),
+        catchCallback = (error) => console.error('Failed', error),
+        successCallback = (data) => console.log('Fetched', data),
+        endCallback = () => console.log(`${url} Stop fetching`),
+        responseTransformFn = 'json',
+        ...fetchOptions
     } = options
 
     startCallback()
-
-    return fetch(url, )
-
+    return fetch(url, fetchOptions)
+        .then((response) => {
+            if (response.status < 200 || response.status > 299) {
+                throw new Error(response.status)
+            }
+            return response
+        })
+        .then((response) => response[responseTransformFn]())
+        .then(successCallback)
+        .then(catchCallback)
+        .finally(endCallback)
 }
-
 export default fetchData
